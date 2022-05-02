a6bc096e49b1f5efa81139fb22d24e3b2cd0c6a5
Klemens Käsznar <klemens.kaesznar@gmail.com>
2022-05-02 12:04:44 +0200

    Encapsulate quality control from plugin

--- src/main/java/at/kk/msc/hcov/sdk/crowdsourcing/platform/ICrowdsourcingConnectorPlugin.java
+++ src/main/java/at/kk/msc/hcov/sdk/crowdsourcing/platform/ICrowdsourcingConnectorPlugin.java
@@ -1,10 +1,11 @@
 package at.kk.msc.hcov.sdk.crowdsourcing.platform;
 
-import at.kk.msc.hcov.sdk.crowdsourcing.platform.model.PublishedTaskIdMappings;
 import at.kk.msc.hcov.sdk.plugin.ConfigurablePlugin;
 import at.kk.msc.hcov.sdk.plugin.PluginConfigurationNotSetException;
 import at.kk.msc.hcov.sdk.verificationtask.model.VerificationTask;
 import java.util.List;
+import java.util.Map;
+import java.util.UUID;
 
 public interface ICrowdsourcingConnectorPlugin extends ConfigurablePlugin<String> {
 
@@ -12,9 +13,9 @@ public interface ICrowdsourcingConnectorPlugin extends ConfigurablePlugin<String
    * Publishes a given set of verification tasks. Requires a configuration to be set early by calling setConfiguration().
    *
    * @param verificationTasks a List of Verification tasks to be published
-   * @return returns the results of the published verificaiton tasks
+   * @return a Map of IDs provided by crowdsourcing platform keyed with the internal extracted element Ids.
    * @throws PluginConfigurationNotSetException if the required configuration for the crowdsourcing platform is not set.
    */
-  PublishedTaskIdMappings publishTasks(List<VerificationTask> verificationTasks) throws PluginConfigurationNotSetException;
+  Map<UUID, String> publishTasks(List<VerificationTask> verificationTasks) throws PluginConfigurationNotSetException;
 
 }
